faa89318917a193f6562713689f5e07026178796
jordangarcia <jordan.garcia@gmail.com>
2013-03-26 20:28:13 -0700

    save

--- bin/huddler_current_community.php
+++ bin/huddler_current_community.php
@@ -4,6 +4,6 @@
  * Outputs: database host and name, separated by \n
  */
 
-include $_SERVER['PROJECTS'] . "/huddler/config/sites/logan.huddler.com/conf/config.db.php";
+include $_SERVER['PROJECTS'] . "/huddler/config/sites/jordan.huddler.com/conf/config.db.php";
 
 echo join("\n", array(COMMUNITY_DB_HOST, COMMUNITY_DB_NAME));
