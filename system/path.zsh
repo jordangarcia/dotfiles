35fa648019cb0546aea0ece0a6f7fde97cef0852
jordangarcia <jordan.garcia@gmail.com>
2013-11-29 15:23:42 -0800

    Major VIMRC cleanup

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,5 @@
 export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="/usr/local/share/npm/bin:$PATH"
 export PATH="$PATH:$HOME/.rbenv/bin" #add rbenv
 export PATH="$PATH:$HOME/bin"
 export PATH="$PATH:/usr/local/go/bin"
